25de354dd80ef0d775246a34abc79b066d7ffd08
principlebrothers <principlesbrothers@yahoo.com>
2022-07-08 13:38:40 +0000

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -11,18 +11,21 @@ const renderUI = () => {
   }
   // Display List of tasks in UI
   populateUI.displayList();
-  // Event for Checkboxes
+  
+  // Set up checkbox event
   const checkboxes = document.querySelectorAll('.checkbox');
 
   [...checkboxes].forEach((button) => {
     button.addEventListener('change', completed);
   });
+
   // Event for Update Task
   const inputs = document.querySelectorAll('.description');
   [...inputs].forEach((input) => {
     input.addEventListener('focusout', storage.updateTask);
   });
-  // Hide Ellipsis icon and display trash icon
+
+  // Icon and Trash can toggle
   [...inputs].forEach((input) => {
     input.addEventListener('focus', (event) => {
       event.target.style.backgroundColor = '#fff4bf';
@@ -31,6 +34,7 @@ const renderUI = () => {
       event.target.nextSibling.nextSibling.classList.remove('hideellipsis');
     });
   });
+
   // Trash task from list
   const trashes = document.querySelectorAll('.fa-trash');
   [...trashes].forEach((trash) => {
@@ -42,10 +46,11 @@ const renderUI = () => {
     });
   });
 };
+
 // Add Item in the list when user press enter after input
 const input = document.querySelector('#additem');
 input.addEventListener('keyup', (event) => {
-  if (event.keyCode === 13) { // Returns Enter Key
+  if (event.keyCode === 13) { 
     storage.add(event);
     event.target.value = '';
     const oldList = document.querySelectorAll('.todoItem');
@@ -53,6 +58,7 @@ input.addEventListener('keyup', (event) => {
     renderUI();
   }
 });
+
 // Remove Completed Tasks
 const btn = document.querySelector('.clearAll-btn');
 btn.addEventListener('click', (e) => {
